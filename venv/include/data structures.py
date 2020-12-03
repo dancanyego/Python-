4a61afb6187ebfe669466ff8140f33a0581494a7
dancanyego <dancanyego76@gmail.com>
2020-12-03 10:44:08 +0300

    This About Data structures

---
+++ venv/include/data structures.py
@@ -0,0 +1,18 @@
+'''
+python data structures that are commonly used
+
+1. Strings
+2. Lists
+3. Tuples
+4. Dictionary
+5. Set
+
+#collection datatypes
+'''
+
+## Strings
+
+first_string = "Heey Everyone "
+print(first_string)
+
+## How to access the Character of a String...??
